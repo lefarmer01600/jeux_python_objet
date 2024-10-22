5b2c1d1e9b90043b4c53a9ddad02aa6fc9161687
MaximeCHANEL <max.chanel01@gmail.com>
2024-10-22 14:28:50 +0200

    ajout class inventaire pour ajouter un objet dans l'inventaire et ou supprimer un objet dans l'inventaire

---
+++ src/personnage/inventaire.py
@@ -0,0 +1,14 @@
+from random import *
+
+class inventaire:
+
+    def __init__(self, liste_inventaire):
+        self.liste_inventaire = []
+    
+    def ajouter_objet(self, liste_inventaire):
+        self.liste_inventaire.append()
+        print(self.liste_inventaire)    
+
+    def supprimer_objet(self, liste_inventaire):
+        self.liste_inventaire.remove()
+        print(self.liste_inventaire)
