d2c5bdfffaa494a369bce43009ac1713302b03ef
Christian <christiancallof74@gmail.com>
2024-12-17 13:04:59 +0100

    more events +

--- src/histoire/list.py
+++ src/histoire/list.py
@@ -7,21 +7,23 @@ prairie = lieu("prairie", "Une prairie verdoyante et paisible où les monstres s
 tmp = histoire("Le chemin ce sépare en deux, vous avez deux options")
 tmp.add_evenement(event("fôret", "pénetrer dans la fôret", "zone", "change_zone"))
 tmp.add_evenement(event("prairie", "rester dans la prairie", "zone", "change_zone"))
+tmp.add_evenement(event("blessure", "Vous marchez sur un piège dissimulé dans l'herbe haute.", "malus", "pv-20"))
+tmp.add_evenement(event("embuscade", "Des bandits surgissent des buissons pour vous attaquer !", "combat", "combat_ennemi_boss"))
 prairie.add_histoire(tmp)
 
 tmp = histoire("Le chemin ce sépare en deux, vous avez deux options")
 tmp.add_evenement(event("fôret", "pénetrer dans la fôret", "zone", "change_zone"))
 tmp.add_evenement(event("prairie", "rester dans la prairie", "zone", "change_zone"))
-
-tmp.add_evenement(event("coffre mystère", "Vous trouvez un coffre entouré d'une aura étrange.", "bonus", "add_object_random"))
-tmp.add_evenement(event("embuscade", "Des bandits surgissent des buissons pour vous attaquer !", "combat", "combat_ennemi_boss"))
-tmp.add_evenement(event("sorcier", "Un sorcier vous propose un marché étrange en échange de 50 pièces d'or.", "marchand", "gold-50, gain_potion_rare"))
-tmp.add_evenement(event("ruines", "Vous trouvez une entrée menant à d'anciennes ruines.", "exploration", "nouvelle_zone_ruines"))
-tmp.add_evenement(event("blessure", "Vous marchez sur un piège dissimulé dans l'herbe haute.", "malus", "pv-20"))
-tmp.add_evenement(event("pnj_rencontre", "Vous croisez un voyageur qui semble avoir des informations intéressantes.", "histoire", "débloque quête annexe"))
-tmp.add_evenement(event("tempête", "Une tempête surgit, réduisant votre visibilité.", "malus", "vision-10, vitesse-20"))
 tmp.add_evenement(event("portail", "Un portail apparaît devant vous, vibrant d'énergie.", "zone", "change_zone_portail"))
+
+Ruines = lieu("Ruines", "Les ruines d'un ancien château, ayant vécu de nombreuses batailles")
+tmp.add_evenement(event("ruines", "Vous trouvez une entrée menant à d'anciennes ruines.", "exploration", "nouvelle_zone_ruines"))
+tmp.add_evenement(event("sorcier", "Un sorcier vous propose un marché étrange en échange de 50 pièces d'or.", "marchand", "gold-50, gain_potion"))
 tmp.add_evenement(event("dragon", "Vous apercevez un dragon endormi au loin. Tenter de s'approcher ?", "risque", "combat_dragon, loot_légendaire"))
+tmp.add_evenement(event("coffre mystère", "Vous trouvez un coffre entouré d'une aura étrange.", "bonus", "add_object_random"))
+
+
+
 
 print(tmp.get_text())
 for evenement in tmp.get_evenements():
