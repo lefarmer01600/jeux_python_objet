46e2b9ccac897c7f0efddb375bbff5c93459a1a1
JeromeHtz <rejome.htz@gmail.com>
2024-12-17 15:00:37 +0100

    implémentation - implémentation évènement

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from src.histoire import lieux, event, histoire
 
 import tkinter as tk
 import json
-import random
 
 json_lieu_foret = "lieux/foret.json"
 json_lieu_maisonAbandonnee = "lieux/maison_abandonnee.json"
 json_lieu_montagne = "lieux/montagne.json"
 json_lieu_prairie = "lieux/prairie.json"
-# test = event.event()
+
 def loadJson(file_path) :
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
@@ -18,20 +17,12 @@ DictForet = loadJson(json_lieu_foret)
 DictMaisonAbandonnee = loadJson(json_lieu_maisonAbandonnee)
 DictMontagne = loadJson(json_lieu_montagne)
 DictPrairie = loadJson(json_lieu_prairie)
-print("\n")
-print("forêt:", DictForet['description'])
 
 # fenêtres
 root = tk.Tk()
 root.title("Jeu à Choix Multiples")
-FrameHistoire = tk.Tk()
-FrameHistoire.title("Interactions")
-
-
-root.title("Interaction")
-
-def selectPlace(DictLieu) :
-    return DictLieu['description']
+Histoire = tk.Tk()
+Histoire.title("Interactions")
 
 # # Question et options
 # question = "Quelle est la capitale de la France?"
@@ -59,33 +50,44 @@ def createFrame(fenetre) :
     return text_frame, scrollbar, text
 
 (frame1, scrollbar1, text1) = createFrame(root)
-(frame2, scrollbar2, text2) = createFrame(FrameHistoire)
+(frame2, scrollbar2, text2) = createFrame(Histoire)
 
 def _event(evenement):
     evt = event.event(evenement['nom'], evenement['description'], evenement['type'], evenement['caracteristiques'])
     return evt
 
+def Event(Evt) :
+    text2.insert("1.0", "Description : "+Evt['description']+"\n")
+    text2.insert("1.0", "Type : "+Evt['type']+"\n")
+    text2.insert("1.0", "--------------------------------")
+
+def testEvent(Evt) :
+    j=0
+    lengthEvt = len(Evt)
+    radio_button = None
+    while j<lengthEvt and radio_button == None :
+        radio_button = tk.Button(Histoire, text=Evt[j]['description'], command=lambda var=Evt[j]:Event(var))
+        radio_button.pack(pady=20)
+        j+=1
+    return
+
+ 
 def displayPlace(DictLieu, lieu):
     text1.insert("1.0", lieu.get_description()+"\n\n")
     lieu.add_histoire(DictLieu)
     hist = lieu.get_random_histoire()
     hist = histoire.histoire(hist['description'])
     for i in range(len(DictLieu['histoire'])):
-        for j in range(len(DictLieu['histoire'][i]['evenement'])):
-            e = _event(DictLieu['histoire'][i]['evenement'][j])
-            text2.insert("1.0", e.get_name()+"\n\n")
+        #e = _event(DictLieu['histoire'][i]['evenement'])
+#       text2.insert("1.0", e.get_name()+"\n\n")
+        testEvent(DictLieu['histoire'][i]['evenement'])
+        # text2.insert("1.0", DictLieu['histoire'][i]['texte'])
 
 # # Créer les boutons radio pour les options
 for i in range(len(options)):
     lieu = lieux.lieu(options[i]['nom'], options[i]['description'])
     radio_button = tk.Button(root, text=texteBoutons[i], command=lambda opt=options[i], lieu=lieu: displayPlace(opt,lieu))
     radio_button.pack(pady=20)
-    
 
-
-# # Bouton pour soumettre la réponse
-# boutonForet = tk.Button(root, text="Entrer dans la forêt", command=selectPlace(DictForet))
-# boutonForet.pack(pady=20)
-
-# # Lancer la boucle principale
 root.mainloop()
+Histoire.mainloop()
